982acc825717f86fc516c03b8ee624cfc3512792
DioguinhoSantos <diiogosantos@live.com>
2022-07-21 18:10:48 -0300

    Barbosa-A-Diogo-Santos

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+
+let primeiroNumero = prompt("Insira um número");
+let segundoNumero = prompt("Insira segundo número");
+
+console.log(primeiroNumero);
+console.log(segundoNumero);
+
+console.log("O primeiro numero é maior que o segundo?", primeiroNumero > segundoNumero);
+console.log("O primeiro numero é igual ao segundo?", primeiroNumero === segundoNumero);
+console.log("O primeiro numero é divisível pelo segundo?", primeiroNumero % segundoNumero);
+console.log("O segundo numero é divisível pelo primeiro?", segundoNumero % primeiroNumero);
+
